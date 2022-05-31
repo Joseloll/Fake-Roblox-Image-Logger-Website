a1b951800f4d82b7083c110b55fcc2747e7dc7ec
Joseloll <106576578+Joseloll@users.noreply.github.com>
2022-05-31 10:50:46 -0600

    Update embed.bat

--- embed.bat
+++ embed.bat
@@ -1,8 +1,8 @@
 @echo off
 title Embed Webhook Url
 color 04
-chcp 65001>nul
 :Main
+chcp 65001>nul
 echo.
 echo  @@@@@@@@ @@@@@@@@@@  @@@@@@@  @@@@@@@@ @@@@@@@  
 echo  @@!      @@! @@! @@! @@!  @@@ @@!      @@!  @@@ 
@@ -13,8 +13,20 @@ echo.
 echo Made By JoseYt#4179
 set /p webhook="Enter Webhook: "
 cls
+echo  @@@@@@@@ @@@@@@@@@@  @@@@@@@  @@@@@@@@ @@@@@@@  
+echo  @@!      @@! @@! @@! @@!  @@@ @@!      @@!  @@@ 
+echo  @!!!:!   @!! !!@ @!@ @!@!@!@  @!!!:!   @!@  !@! 
+echo  !!:      !!:     !!: !!:  !!! !!:      !!:  !!! 
+echo  : :: ::   :      :   :: : ::  : :: ::  :: :  : 
+echo.
 set /p username="Enter Username: "
 cls
+echo  @@@@@@@@ @@@@@@@@@@  @@@@@@@  @@@@@@@@ @@@@@@@  
+echo  @@!      @@! @@! @@! @@!  @@@ @@!      @@!  @@@ 
+echo  @!!!:!   @!! !!@ @!@ @!@!@!@  @!!!:!   @!@  !@! 
+echo  !!:      !!:     !!: !!:  !!! !!:      !!:  !!! 
+echo  : :: ::   :      :   :: : ::  : :: ::  :: :  : 
+echo.
 set /p message="Enter Message: "
 cls
 curl -d "content=%message%" -d "username=%username%" -X POST %webhook%
